b0826c7ad550d30eb13d3bb1af8264d81375d15b
Animesh <78248437+waterManAni@users.noreply.github.com>
2021-07-08 09:08:07 +0800

    Update Example batch statement.bat

--- Example batch statement.bat
+++ Example batch statement.bat
@@ -1,7 +1,7 @@
 ::@echo off
 
-set tcf="C:\Users\Animesh.Paudel\Desktop\Templates and Tools\python and Arcpy tools\Tuflow_post processor\test\OAR_RTIO.tcf"
-set resultsfolder="C:\Users\Animesh.Paudel\Desktop\Templates and Tools\python and Arcpy tools\Tuflow_post processor\test"
-set asctoasc="T:\animesh.paudel\asc_to_asc.2020-10-AA\asc_to_asc_w64.exe"
+set tcf="linktotcffile.tcf"
+set resultsfolder="link to where ensemble results are saved, don't end the folder path with backslash"
+set asctoasc="\asc_to_asc.2020-10-AA\asc_to_asc_w64.exe"
 
-call "T:\animesh.paudel\Python\Tuflow_postprocess\PostProcess_WinPy.bat"
+call "PostProcess_WinPy.bat"
